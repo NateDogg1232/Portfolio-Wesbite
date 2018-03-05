70fac50629beef9c0041334e7f2b2449f9a386d3
NateDogg1232 <nathanielbarragan@gmail.com>
2018-03-05 15:20:02 -0800

    I did way too much

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 //Put all global functions in here
+
+var hamburgerMenuIsOpen = false
+function onHamburgerClick() {
+    hamburger = document.getElementById("hamburger");
+    hamburgerMenu = document.getElementById("hamburger-menu");
+    hamburgerMenu.style.width = "100px";
+    hamburgerMenu.style.marginLeft = "0px";
+    hamburgerMenu.style.visibility = "visible";
+    
+    
+    hamburger.style.left = "100px";
+}
