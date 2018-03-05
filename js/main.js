35f26e70ee474807dd0e75b2de21e71a687c2bed
NateDogg1232 <nathanielbarragan@gmail.com>
2018-03-05 15:29:07 -0800

    Added more stuff

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 //Put all global functions in here
 
 var hamburgerMenuIsOpen = false
+
+var hamburgerStyling = hamburger = document.getElementById("hamburger").style;
+var hamburgerMenuStyling = document.getElementById("hamburger-menu").style;
+
 function onHamburgerClick() {
-    hamburger = document.getElementById("hamburger");
-    hamburgerMenu = document.getElementById("hamburger-menu");
-    hamburgerMenu.style.width = "100px";
-    hamburgerMenu.style.marginLeft = "0px";
-    hamburgerMenu.style.visibility = "visible";
-    
-    
-    hamburger.style.left = "100px";
+    if (!hamburgerMenuIsOpen) {
+        hamburger = document.getElementById("hamburger");
+        hamburgerMenu = document.getElementById("hamburger-menu");
+        hamburgerMenu.style.width = "100px";
+        hamburgerMenu.style.marginLeft = "0px";
+        hamburgerMenu.style.visibility = "visible";
+
+
+        hamburger.style.left = "100px";
+    }
 }
