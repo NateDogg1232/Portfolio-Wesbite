ba1ccc71cc37a8c0d953c39465bdaabec125caf2
NateDogg1232 <nathanielbarragan@gmail.com>
2018-03-05 13:02:09 -0800

    Improved the SASS compiling script

--- compilesass.js
+++ compilesass.js
@@ -1,18 +1,27 @@
 //Made for node.js
 var sass = require('node-sass');
 var fs = require('fs');
-sass.render({
-    file: "main.scss",
-    outFile: "main.css"
-}, function (err, result) {
-    if (err) {
-        console.log("error");
-        console.log(err);
-        return;
-    }
-    fs.writeFile("main.css", result.css, function (err) {
-        if (!err) {
-            console.log("No errors");
+const cssFolder = "css"
+const sassFolder = "sass"
+
+function compile(file) {
+    sass.render({
+        file: sassFolder + "/" + file + ".scss",
+        outFile: cssFolder+ "/" + file + ".css"
+    }, function (err, result) {
+        if (err) {
+            console.log("error");
+            console.log(err);
+            return;
         }
+        fs.writeFile(cssFolder + "/" + file + ".css", result.css, function (err) {
+            if (!err) {
+                console.log("No errors");
+            } else {
+                console.log(err);
+            }
+        });
     });
-});
+}
+
+compile("index");
