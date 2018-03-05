e9dc6c24e6e262eeb779dc42d9604220a9277817
NateDogg1232 <nathanielbarragan@gmail.com>
2018-03-05 13:17:29 -0800

    Did more work with the compiling script

--- compilesass.js
+++ compilesass.js
@@ -7,7 +7,7 @@ const sassFolder = "sass"
 function compile(file) {
     sass.render({
         file: sassFolder + "/" + file + ".scss",
-        outFile: cssFolder+ "/" + file + ".css"
+        outFile: cssFolder + "/" + file + ".css"
     }, function (err, result) {
         if (err) {
             console.log("error");
@@ -24,4 +24,16 @@ function compile(file) {
     });
 }
 
-compile("index");
+//Iterate through all the sass files in the
+fs.readdir("sass/", function (err, items) {
+
+            for (var i = 0; i < items.length; i++) {
+                let item = items[i].toString();
+                if (item.substr(item.length - 5) == ".scss") {
+                    //Check to make sure that it's not a library file
+                    if (item.substring(0, 1) != "_") {
+                        console.log("Compiling " + item);
+                        compile(item.substring(0, item.length - 5));
+                    }
+                }
+            }});
