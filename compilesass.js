b09f8a2e36186c361b9734e52ba92bf66e93bc35
NateDogg1232 <nathanielbarragan@gmail.com>
2018-03-06 13:39:40 -0800

    Adding the compiling stuff and trying blur

--- compilesass.js
+++ compilesass.js
@@ -4,9 +4,12 @@ var fs = require('fs');
 const cssFolder = "css";
 const sassFolder = "sass";
 
+var fsTimeout = false;
+
 console.log(new Date());
 
 function compile(file) {
+    console.log("Compiling" + file);
     sass.render({
         file: sassFolder + "/" + file + ".scss",
         outFile: cssFolder + "/" + file + ".css"
@@ -28,16 +31,42 @@ function compile(file) {
 
 //TODO: Make a watcher to check for new files and file changes
 
-//Iterate through all the sass files in the
-fs.readdir("sass/", function (err, items) {
+function fileChanged(eventType, fileName) {
+    if (!fsTimeout) {
+        if (eventType == "change") {
+            if (fileName.substr(fileName.length - 5) == ".scss") {
+                if (fileName.substring(0, 1) == "_") {
+                    //This is a library file, so we need to compile all files
+                    console.log("Compile all files");
+                    //Iterate through each file, and compile them
+                    fs.readdir("sass/", function (err, items) {
 
-            for (var i = 0; i < items.length; i++) {
-                let item = items[i].toString();
-                if (item.substr(item.length - 5) == ".scss") {
-                    //Check to make sure that it's not a library file
-                    if (item.substring(0, 1) != "_") {
-                        console.log("Compiling " + item);
-                        compile(item.substring(0, item.length - 5));
-                    }
+                        for (var i = 0; i < items.length; i++) {
+                            let item = items[i].toString();
+                            if (fileName.substr(item.length - 5) == ".scss") {
+                                //Check to make sure that it's not a library file
+                                if (item.substring(0, 1) != "_") {
+                                    console.log("Compiling " + item);
+                                    compile(item.substring(0, item.length - 5));
+                                }
+                            }
+                        }
+                    });
+                } else {
+                    compile(fileName.substring(0, fileName.length - 5));
                 }
-            }});
+            }
+        }
+        fsTimeout = true;
+        setTimeout(function () {
+            fsTimeout = false;
+        }, 1000);
+    }
+}
+
+
+//Watch the directory
+fs.watch("sass/", {
+    recursive: true,
+    encoding: "utf8"
+}, fileChanged);
--- js/main.js
+++ js/main.js
@@ -2,32 +2,19 @@
 
 var hamburgerMenuIsOpen = false
 
-var hamburgerStyling = hamburger = document.getElementById("hamburger").style;
-var hamburgerMenuStyling = document.getElementById("hamburger-menu").style;
+var hamburgerClass = document.getElementById("hamburger").getAttribute("class");
+
 
 function onHamburgerClick() {
     hamburger = document.getElementById("hamburger");
     hamburgerMenu = document.getElementById("hamburger-menu");
-    hamburger.classList.toggle("x");
     if (!hamburgerMenuIsOpen) {
-        hamburgerMenu.style.width = "300px";
-        hamburgerMenu.style.marginLeft = "0px";
-        hamburgerMenu.style.visibility = "visible";
-
-
-        hamburger.style.left = "300px";
-        //translate(-10px, 8px)
-        document.getElementById("bar1").style.transform = "rotate(-45deg) translate(-9px, 6px)";
-        document.getElementById("bar2").style.opacity = "0";
-        //translate(-20px, -20px)
-        document.getElementById("bar3").style.transform = "rotate(45deg) translate(-8px, -8px)";
+        hamburger.setAttribute("class", "transition");
+        hamburgerMenu.setAttribute("class", "transition");
         hamburgerMenuIsOpen = true;
     } else {
         hamburgerMenuIsOpen = false;
-        hamburger.style = hamburgerStyling;
-        document.getElementById("bar1").style.transform = "";
-        document.getElementById("bar2").style.opacity = "1";
-        document.getElementById("bar3").style.transform = "";
-        hamburgerMenu.style = hamburgerMenuStyling;
+        hamburger.setAttribute("class", hamburgerClass);
+        hamburgerMenu.setAttribute("class", "");
     }
 }
