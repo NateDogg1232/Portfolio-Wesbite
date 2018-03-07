de1db59b6de2e9dd717a17a6956659f7e0fd7ee3
NateDogg1232 <nathanielbarragan@gmail.com>
2018-03-06 19:34:45 -0800

    Added cards

--- js/main.js
+++ js/main.js
@@ -7,23 +7,20 @@ var hamburgerClass = document.getElementById("hamburger").getAttribute("class");
 
 
 function onHamburgerClick() {
-    hamburger = document.getElementById("hamburger");
-    hamburgerMenu = document.getElementById("hamburger-menu");
-    if (!hamburgerMenuIsOpen) {
-        hamburger.setAttribute("class", "transition");
-        hamburgerMenu.setAttribute("class", "transition");
-        hamburgerMenuIsOpen = true;
-        hamburgerTimeout = setTimeout(function () {
-            if (hamburgerMenuIsOpen) {
-                hamburgerMenu.setAttribute("class", "");
-                hamburger.setAttribute("class", "");
-                hamburgerMenuIsOpen = false;
-            }
-        }, 5000);
-    } else {
-        hamburgerMenuIsOpen = false;
-        hamburger.setAttribute("class", "");
-        hamburgerMenu.setAttribute("class", "");
-        clearTimeout(hamburgerTimeout);
-    }
+        hamburger = document.getElementById("hamburger");
+        hamburgerMenu = document.getElementById("hamburger-menu");
+        if (!hamburgerMenuIsOpen) {
+            hamburger.setAttribute("class", "transition");
+            hamburgerMenu.setAttribute("class", "transition");
+            hamburgerMenuIsOpen = true;
+        } else {
+            hamburgerMenuIsOpen = false;
+            hamburger.setAttribute("class", "");
+            hamburgerMenu.setAttribute("class", "");
+            clearTimeout(hamburgerTimeout);
+        }
+}
+
+function convert24HourTo12Hour(time) {
+
 }
