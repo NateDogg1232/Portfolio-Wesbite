cf9bb220cce7f941ea18938c046401a02a27f811
NateDogg1232 <nathanielbarragan@gmail.com>
2018-03-14 15:28:59 -0700

    I fixed the cards

--- js/contact.js
+++ js/contact.js
@@ -1,62 +1,46 @@
 var cards = document.getElementsByClassName("card");
 const cardMax = cards.length - 1;
-const transitionTime = "1s";
-var marginLeftOffScreen = "-2000px";
-var marginRightOffScreen = "2000px";
 var cardTop = cards[0].getBoundingClientRect().top;
 var currentCard = 0;
 
 //Initialize all the cards
-for (let i = 1; i <= cardMax; i++) {
+for (let i = 0; i <= cardMax; i++) {
     cards[i].style.top = cardTop;
-    cards[i].style.marginLeft = marginLeftOffScreen;
+    cards[i].style.transitionDuration = "1s";
 }
-cards[0].style.top = cardTop;
-cards[0].style.margin = "0px";
-cards[0].style.transitionDuration = "1s";
-cards[cardMax].marginLeft = marginRightOffScreen;
+
+updateCardCount();
 
 
 function cardLeft() {
-    //Move the current card off the screen to the left
-    cards[currentCard].style.transitionDuration = transitionTime;
-    cards[currentCard].style.marginLeft = marginLeftOffScreen;
-    //Move the next card to the right side
-    getNextCard().style.transitionDuration = "0s";
-    getNextCard().style.marginLeft = marginRightOffScreen;
     currentCard--;
-    if (currentCard < 0) {
-        currentCard = cardMax;
+    if (currentCard <= 0) {
+        document.getElementById("leftarrow").classList.add("disabled");
+        currentCard = 0;
     }
-    console.log(currentCard);
-    cards[currentCard].style.transitionDuration = transitionTime;
-    cards[currentCard].style.marginLeft = "0px";
-}
-function getNextCard() {
-    if (currentCard == cardMax) {
-        return cards[0];
-    }
-    return cards[currentCard + 1];
-}
-function getPreviousCard() {
-    if (currentCard == 0) {
-        return cards[cardMax];
-    }
-    return cards[currentCard - 1];
+    document.getElementById("rightarrow").classList.add("enabled");
+    cards[currentCard].classList.add("center");
+    cards[currentCard].classList.remove("right");
+    cards[currentCard + 1].classList.add("left");
+    cards[currentCard + 1].classList.remove("center");
+    updateCardCount();
 }
 
 function cardRight() {
-    //Move the current card off the screen to the right
-    cards[currentCard].style.transitionDuration = transitionTime;
-    cards[currentCard].style.marginLeft = marginRightOffScreen;
-    //Move the next card to the left side
-    getPreviousCard().style.transitionDuration = "0s";
-    getPreviousCard().style.marginLeft = marginLeftOffScreen;
     currentCard++;
-    if (currentCard > cardMax) {
-        currentCard = 0;
+    if (currentCard >= cardMax) {
+        document.getElementById("rightarrow").classList.add("disabled");
+        currentCard = cardMax;
     }
-    console.log(currentCard);
-    cards[currentCard].style.transitionDuration = transitionTime;
-    cards[currentCard].style.marginLeft = "0px";
+    document.getElementById("leftarrow").classList.add("enabled");
+    cards[currentCard].classList.add("center");
+    cards[currentCard].classList.remove("left");
+    cards[currentCard - 1].classList.add("right");
+    cards[currentCard - 1].classList.remove("center");
+    updateCardCount();
 }
+
+function updateCardCount() {
+    document.getElementById("cardcount").innerHTML = "Card " + (currentCard + 1) + " of " + (cardMax + 1);
+}
+
